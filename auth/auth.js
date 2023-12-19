845b2984b07ae7e649acd644bb2473263ef83351
B-AJ-Amar <amarbouakaz91@gmail.com>
2023-12-19 21:59:26 +0100

    add auth functions

--- auth/auth.js
+++ auth/auth.js
@@ -1,22 +1,61 @@
 const crypto = require("crypto");
+const jwt = require('jsonwebtoken');
 
+const {session, driver} = require('../config/db.js');
 
+const jwtSecret = 'e0fb385c27f800fbb82fce3d7515c82e116277e25a95dc2a3d399162f220ece6431be2ebe112316165f4859d9cbdfd85f16c903951971dddb8a9d92d614e6b0c' ;
 
-function make_password(password) {
-    if (password == null){ return 0;}
-    return crypto.createHash('md5').update(String(password)).digest('hex');
+// ? JWT AUTHENTICATION ==========================================================
+
+function generateAccessToken(username) {
+    return jwt.sign(username,jwtSecret, { expiresIn: '1800s' });
 }
+
+function isAuthenticated(req, res, next) {
+    // cookies based authentication
+    const token = req.cookies['access-token'];
+    if (token == null) return res.sendStatus(401);
+    jwt.verify(token, jwtSecret, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user; // like request.user in django
+        next();
+    })
+}
+
+    
+function isLoggedin(req, res, next) {
+    if (req.user) next();
+    else res.redirect('/login');
+}
+// list of login required midelewares
+const loginRequired = [isAuthenticated, isLoggedin];
 
 
 function authenticate(username,password) {
     if (password == null || username == null) return 0;
     var password = String(password);
     var username = String(username);
-
-    
+    var query = `MATCH (user:User {username:"${username}", password:"${ crypto.createHash('md5').update(password).digest('hex')}"}) RETURN user`;
+    return session.run(query).then(result => {
+        if (result.records.length == 0) return null;
+        else{
+            console.log(result.records[0].get(0).properties);
+            return result.records[0].get(0).properties;  
+        } 
+    });  
 }
+// hash password with md5 ======================================================
+function make_password(password) {
+    if (password == null){ return 0;}
+    return crypto.createHash('md5').update(String(password)).digest('hex');
+}
+
+
 
 module.exports = {
     make_password,
-    authenticate
+    authenticate,
+    isAuthenticated,
+    generateAccessToken,
+    loginRequired
 }
--- config/db.js
+++ config/db.js
@@ -1,6 +1,12 @@
 const neo4j = require('neo4j-driver');
+// const sqlite3 = require('sqlite3').verbose()
 
+// neo4j configuration ========================================================
 const driver = neo4j.driver('bolt://localhost:7687');
 const session = driver.session();
+
+// sqlite3 configuration ======================================================
+// const sqlDB = new sqlite3.Database('./db.sqlite3')
+
 
 module.exports = { session, driver };
