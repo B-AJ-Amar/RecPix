525e509179a1ff544918f5afa21ef0ec6e9314b2
B-AJ-Amar <amarbouakaz91@gmail.com>
2024-01-14 22:03:23 +0100

    lern graphql PT1

--- backend/graphql/resolvers.js
+++ backend/graphql/resolvers.js
@@ -1,7 +1,14 @@
+
 const resolvers = {
     hello: () => {
       return 'world';
     },
+    random:() =>{
+      return Math.random();
+    },
+    rollDice:args =>{
+      return [args.numSides,args.numDice]
+    }
   };
   
   module.exports = resolvers;
--- backend/graphql/schema.js
+++ backend/graphql/schema.js
@@ -3,6 +3,9 @@ const { buildSchema } = require('graphql');
 const schema = buildSchema(`
   type Query {
     hello: String
+    random: Float!
+    rollDice(numDice: Int!, numSides: Int): [Int]
+    
   }
 `);
 
